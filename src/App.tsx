26e4b5f40a17620321a8b9fdb8d6dfdec181a3e7
bikram993298 <bikrambarman993298@gmail.com>
2024-12-17 00:56:10 +0530

    #1 resolved.desighned the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CampaignAutomation from "./pages/CampaignAutomation";
+
 //import Personalization from './components/Personalization';
 
 const App = () => {
   return (
     <>
-    <CampaignAutomation/>
+    <Home/>
     </>
   )
 };
